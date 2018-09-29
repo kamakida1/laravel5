122dd5f5d5c0f9c709e6e73eb7c65a66aadfb7be
kritsana khemnoo <kritsana.mana303@gmail.com>
2018-09-30 03:34:56 +0700

    add

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+    'facebook' => [
+      'client_id' => '333138317435146',         // Your GitHub Client ID
+      'client_secret' => 'a19a1e9556461714a9c06a250282ac2a',
+      // Your GitHub Client Secret
+      'redirect' => 'http:localhost:8000/login/facebook/callback',
+  ],
+
+];
